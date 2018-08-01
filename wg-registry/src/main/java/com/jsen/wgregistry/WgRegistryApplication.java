a2cc4bf6c2e3217f098d8baa20c32c93134ad8c4
353682362 <353682362@qq.com>
2018-08-01 16:06:11 +0800

    新建工程WG

---
+++ wg-registry/src/main/java/com/jsen/wgregistry/WgRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.jsen.wgregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgRegistryApplication.class, args);
+	}
+}
---
+++ wg-system/src/main/java/com/jsen/wgzuul/WgZuulApplication.java
@@ -0,0 +1,12 @@
+package com.jsen.wgzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgZuulApplication.class, args);
+	}
+}
