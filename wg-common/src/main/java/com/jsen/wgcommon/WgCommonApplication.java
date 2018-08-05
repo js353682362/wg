58b7662d32a1fe918679dc1d8f65cb009aebcbea
jiaosen <353682362@qq.com>
2018-08-05 17:58:59 +0800

    代码修改提交

--- wg-common/src/main/java/com/jsen/wgcommon/WgCommonApplication.java
+++ wg-common/src/main/java/com/jsen/wgcommon/WgCommonApplication.java
@@ -1,12 +1,12 @@
-package com.jsen.config;
+package com.jsen.wgcommon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConfigApplication {
+public class WgCommonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
+		SpringApplication.run(WgCommonApplication.class, args);
 	}
 }
---
+++ wg-config/src/main/java/com/jsen/config/WgConfigApplication.java
@@ -0,0 +1,14 @@
+package com.jsen.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class WgConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WgConfigApplication.class, args);
+    }
+}
